9898719dbe5b9ab96e24def69f110cd6715c2751
Sookeyy-12 <thatonesuket@gmail.com>
2024-05-23 17:45:22 +0530

    fixed test scripts

--- Testing/yolotest.py
+++ Testing/yolotest.py
@@ -4,14 +4,23 @@ import cvzone
 import math
 import time
 
-cap = cv2.VideoCapture(0) # for webcam
+cap = cv2.VideoCapture(1) # for webcam
 cap.set(3, 640)
 cap.set(4, 480)
 # cap = cv2.VideoCapture("test.mp4") # for video file
 
 model = YOLO("../models/yolov8n.pt")
 
-classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "tarin", "truck", "boat", "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat", "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella", "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat", "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup", "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli", "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed", "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone", "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors", "teddy bear", "hair drier", "toothbrush"]
+classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
+            "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat", "dog",
+            "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
+            "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite",
+            "baseball bat", "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle",
+            "wine glass", "cup", "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich",
+            "orange", "broccoli", "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa",
+            "pottedplant", "bed", "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote",
+            "keyboard", "cell phone", "microwave", "oven", "toaster", "sink", "refrigerator", "book",
+            "clock", "vase", "scissors", "teddy bear", "hair drier", "toothbrush"]
 
 prev_frame_time = 0
 new_frame_time = 0
@@ -30,9 +39,11 @@ while True:
             w, h = x2-x1, y2-y1
             cvzone.cornerRect(img, (x1, y1, w, h))
             # Confidence
-            conf = math.ceil((box.conf[0]*100))/100
+            conf = math.ceil((box.conf[0] * 100))/100
             # class name
             cls = int(box.cls[0])
+
+            cvzone.putTextRect(img, f'{classNames[cls]} {conf}', (max(0, x1), max(35, y1)), scale = 1, thickness=1)
 
     fps = 1 / (new_frame_time - prev_frame_time)
     prev_frame_time = new_frame_time
